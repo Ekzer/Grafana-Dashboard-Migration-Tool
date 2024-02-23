b400bf5951ddd7c8962cc6fff1143c18f0abb421
Hugo Maitre <hugo.maitre@copper.co>
2024-02-23 18:00:32 +0100

    Better logs

--- src/config.ts
+++ src/config.ts
@@ -18,15 +18,15 @@ export class Config {
     getConfig(env: string, isDestination: boolean = false): EnvConfig {
         const processEnv = process.env
         const apiKey = processEnv[`${env.toUpperCase()}_ENV_API_KEY`]
-        assert.ok(!!apiKey, `Env variable API_KEY for env ${env} was not set`)
+        assert.ok(!!apiKey, `Env variable ${env.toUpperCase()}_ENV_API_KEY is not set`)
 
         const host = processEnv[`${env.toUpperCase()}_ENV_HOST`]
-        assert.ok(!!host, `Env variable HOST for env ${env} was not set`)
+        assert.ok(!!host, `Env variable ${env.toUpperCase()}_ENV_HOST is not set`)
         console.info(`Using Host: ${host}`)
 
         const alertsUids = processEnv[`${env.toUpperCase()}_ENV_ALERTS_UID`]?.split(",")
         if (isDestination) {
-            console.info(`Using Alerts UIDs: ${alertsUids?.join(" ")}`)
+            console.info(`Using Alerts UIDs: ${alertsUids?.join(" ") || "None"}`)
         }
 
         const config: EnvConfig = {
